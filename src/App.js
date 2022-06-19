a07c0ce643ed3a01f14d58beb9914585474164fe
Md. Shariar Kabir <kabircp08@gmail.com>
2022-06-20 05:33:53 +0600

    make some fun with new projecst

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-<Route path="/pie" element={(<PieChart/>)} />
-import {HomeChart,DoughnutChart,PieChart,BarChart,LineChart,TestChart} from './components/index'
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+;<Route path="/pie" element={<PieChart />} />
+import {
+  HomeChart,
+  DoughnutChart,
+  PieChart,
+  BarChart,
+  LineChart,
+  TestChart,
+} from './components/index'
+import { TestUseRef } from './test/index'
+import './App.css'
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <div className="flex w-full" style={{ minHeight:"100vh" }}>
-            <Sidebar/>
-            {/* bg-slate-800 */}
-            <div className='w-full bg-main-dark-bg xh-screen'> 
-                <Routes>
-                  <Route path="/" element={(<HomeChart/>)} />
-                  <Route path="/pie" element={(<PieChart/>)} />
-                  <Route path="/bar" element={(<BarChart/>)} />
-                  <Route path="/line" element={(<LineChart/>)} />
-                  <Route path="/doughnut" element={(<DoughnutChart/>)} />
-                  <Route path="/test-chart" element={(<TestChart/>)} />
-                </Routes>
-            </div>
+        <div className="flex w-full" style={{ minHeight: '100vh' }}>
+          <Sidebar />
+          {/* bg-slate-800 */}
+          <div className="w-full bg-main-dark-bg xh-screen">
+            <Routes>
+              <Route path="/" element={<HomeChart />} />
+              <Route path="/pie" element={<PieChart />} />
+              <Route path="/bar" element={<BarChart />} />
+              <Route path="/line" element={<LineChart />} />
+              <Route path="/doughnut" element={<DoughnutChart />} />
+              <Route path="/test-chart" element={<TestChart />} />
+              {/* My Personal Testing Area*/}
+              <Route path="/test-useref" element={<TestUseRef />} />
+            </Routes>
+          </div>
         </div>
       </BrowserRouter>
     </>
-    
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,58 +1,85 @@
 import React from 'react'
-import { NavLink,Link } from 'react-router-dom'
-import pieIcon from './../icons/icons8-chart-48.png'
+import { NavLink, Link } from 'react-router-dom'
+
 const Sidebar = () => {
   return (
     <>
-      <div className='w-auto  text-center bg-secondary-dark-bg'>
-          <div className='break-words whitespace-nowrap p-5 logo-font mb-10'>
-            <NavLink to="/">ChartJs Dashboard</NavLink>
-          </div>
-          <div className='flex flex-col'>
-              <NavLink to="/pie"
-                className="p-1 ml-2 mr-2 mb-2"
-                style={({isActive}) => ({
-                  color: isActive ? "#fff" : "#cca",
-                  backgroundColor: isActive ? "#454543" : "",
-                  borderRadius:"10px",
-                })}
-              >Pie Chart</NavLink>
-            
-            <NavLink 
-              // className={({isActive}) => isActive ? 'bg-black' : ''}
-              className="p-1 ml-2 mr-2 mb-2"
-              style={({isActive}) => ({
-                color: isActive ? "White" : "#cca",
-                backgroundColor: isActive ? "#454543" : "",
-                borderRadius:"10px",
-              })}
-              to="/bar">Bar Chart</NavLink>
-              <NavLink 
-                className="p-1 ml-2 mr-2 mb-2"
-                style={({isActive}) => ({
-                  color: isActive ? "White" : "#cca",
-                  backgroundColor: isActive ? "#454543" : "",
-                  borderRadius:"10px",
-                })}
-              to="/line">Line Chart</NavLink>
-              <NavLink 
-                className="p-1 ml-2 mr-2 mb-2"
-                style={({isActive}) => ({
-                  color: isActive ? "White" : "#cca",
-                  backgroundColor: isActive ? "#454543" : "",
-                  borderRadius:"10px",
-                })}
-              to="/doughnut">Doughnut Chart</NavLink>
-              <NavLink 
-                className="p-1 ml-2 mr-2 mb-2"
-                style={({isActive}) => ({
-                  color: isActive ? "White" : "#cca",
-                  backgroundColor: isActive ? "#454543" : "",
-                  borderRadius:"10px",
-                })}
-              to="/test-chart">Test Chart</NavLink>
-          </div>
+      <div className="w-auto  text-center bg-secondary-dark-bg">
+        <div className="break-words whitespace-nowrap p-5 logo-font mb-10">
+          <NavLink to="/">ChartJs Dashboard</NavLink>
         </div>
+        <div className="flex flex-col">
+          <NavLink
+            to="/pie"
+            className="p-1 ml-2 mr-2 mb-2"
+            style={({ isActive }) => ({
+              color: isActive ? '#fff' : '#cca',
+              backgroundColor: isActive ? '#454543' : '',
+              borderRadius: '10px',
+            })}
+          >
+            Pie Chart
+          </NavLink>
+
+          <NavLink
+            // className={({isActive}) => isActive ? 'bg-black' : ''}
+            className="p-1 ml-2 mr-2 mb-2"
+            style={({ isActive }) => ({
+              color: isActive ? 'White' : '#cca',
+              backgroundColor: isActive ? '#454543' : '',
+              borderRadius: '10px',
+            })}
+            to="/bar"
+          >
+            Bar Chart
+          </NavLink>
+          <NavLink
+            className="p-1 ml-2 mr-2 mb-2"
+            style={({ isActive }) => ({
+              color: isActive ? 'White' : '#cca',
+              backgroundColor: isActive ? '#454543' : '',
+              borderRadius: '10px',
+            })}
+            to="/line"
+          >
+            Line Chart
+          </NavLink>
+          <NavLink
+            className="p-1 ml-2 mr-2 mb-2"
+            style={({ isActive }) => ({
+              color: isActive ? 'White' : '#cca',
+              backgroundColor: isActive ? '#454543' : '',
+              borderRadius: '10px',
+            })}
+            to="/doughnut"
+          >
+            Doughnut Chart
+          </NavLink>
+          <NavLink
+            className="p-1 ml-2 mr-2 mb-2"
+            style={({ isActive }) => ({
+              color: isActive ? 'White' : '#cca',
+              backgroundColor: isActive ? '#454543' : '',
+              borderRadius: '10px',
+            })}
+            to="/test-chart"
+          >
+            Test Chart
+          </NavLink>
+          {/* Testing Area for me */}
+          <NavLink
+            className="p-1 ml-2 mr-2 mb-2"
+            style={({ isActive }) => ({
+              color: isActive ? 'White' : '#cca',
+              backgroundColor: isActive ? '#454543' : '',
+              borderRadius: '10px',
+            })}
+            to="/test-useref"
+          >
+            Test UseRef
+          </NavLink>
+        </div>
+      </div>
     </>
   )
 }
